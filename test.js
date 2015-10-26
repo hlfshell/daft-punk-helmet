ca0a97b975b22b294030370650af5a15413bf975
Keith Chester <kchester@gmail.com>
2015-10-25 20:27:03 -0500

    fix: Testing teal

--- test.js
+++ test.js
@@ -7,14 +7,15 @@ board.on("ready", function() {
 
     var strip = new pixel.Strip({
         data: 6,
-        length: 4,
+        length: 2,
         board: this,
         controller: "FIRMATA"
     });
 
     strip.on("ready", function() {
         // do stuff with the strip here.
-		console.log("Ready whoo!");
+		strip.color("teal");
+		strip.show();
     });
 
 });
